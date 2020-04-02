d534aca6fb7a9f5387c2e88e57b55251ad2a91b3
teffanymae11 <ph2200009@transcosmos-ph.local>
2020-04-02 13:47:26 +0800

    Convert to Formik

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,17 +1,111 @@
 import React from 'react';
 import { FormVars } from './types'
 
+import { useFormik } from 'formik'
+
+const validate = (values: any) => {
+  const errors = {
+    name: '',
+    height: '',
+    weight: '',
+    gender: '',
+    age: ''
+  };
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length > 50) {
+    errors.name = 'Must be 50 characters or less';
+  }
+
+  if (!values.weight) {
+    errors.weight = 'Required';
+  } else if (values.weight.length > 20) {
+    errors.weight = 'Must be 20 characters or less';
+  }
+
+  if (!values.age) {
+    errors.age = 'Required';
+  } else if (values.age.length >= 3) {
+    errors.age = 'Must enter age';
+  }
+
+
+  return errors;
+};
+
 
 const Form = ({ personalData, onChange, onReset, onSubmit }: FormVars) => {
 
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      height: '',
+      weight: '',
+      gender: '',
+      age: ''
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <form onSubmit={onSubmit} onReset={onReset}>
-      <input type="text" name="name" placeholder="Name" value={personalData.name} onChange={onChange} />
-      <input type="number" name="height" placeholder="Height" value={personalData.height} onChange={onChange} />
-      <input type="number" name="weight" placeholder="Weight" value={personalData.weight} onChange={onChange} />
-      <input type="radio" name="gender" value="m" checked={personalData.gender === "m"} onChange={onChange} /> Male
-        <input type="radio" name="gender" value="f" checked={personalData.gender === "f"} onChange={onChange} /> Female
-        <input type="number" name="age" placeholder="Age" value={personalData.age} onChange={onChange} />
+    <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formik.values.name}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} />
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
+      <input
+        type="number"
+        name="height"
+        placeholder="Height"
+        value={formik.values.height}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} />
+      {formik.touched.height && formik.errors.height ? (
+        <div>{formik.errors.height}</div>
+      ) : null}
+      <input
+        type="number"
+        name="weight"
+        placeholder="Weight"
+        value={formik.values.weight}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} />
+      {formik.touched.weight && formik.errors.weight ? (
+        <div>{formik.errors.weight}</div>
+      ) : null}
+      <input
+        type="radio"
+        name="gender"
+        value="m"
+        checked={formik.values.gender === "m"}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} /> Male
+      <input
+        type="radio"
+        name="gender"
+        value="f"
+        checked={formik.values.gender === "f"}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} /> Female
+      <input
+        type="number"
+        name="age"
+        placeholder="Age"
+        value={formik.values.age}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange} />
+      {formik.touched.age && formik.errors.age ? (
+        <div>{formik.errors.age}</div>
+      ) : null}
       <div>
         <button type="reset" >Reset</button>
         <button type="submit">Add Record</button>
